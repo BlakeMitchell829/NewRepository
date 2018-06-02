d5ae465240de3b8389ff9d2dc2a12f6adae7fc95
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-06-02 10:34:19 -0500

    added UT Bootcamp Homework Folder

---
+++ UT Bootcamp Homework/HTML Javascript.js
@@ -0,0 +1,25 @@
+    [script type="text/javascript" src="HTMLJavascript.html"][/script]
+    
+        var document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+        });
+        
+        var document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").style.background-color = "Blue;";
+
+        });
+        var document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+
+        });
+        
+        var document.getElementById("button4").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "25px";
+
+        });
+        [/script]
